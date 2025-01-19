ff9f5ac67c94d7f9586760a1effe575063b1483c
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2025-01-18 19:03:37 -0500

    complete dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
-import Dashboard from "./ui/dashboard/dashboard";
+import Page from "./app/dashboard/page";
 
 function App() {
   return (
     <>
-      <h1>CineBot</h1>
-      <Dashboard />
+      <Page />
     </>
   );
 }
