fd1ba9be209c866b1f145672150717366b88a943
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2025-01-22 11:21:00 -0500

    Initialized Redux

--- client/util/backendAPICalls.ts
+++ client/util/backendAPICalls.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getMovies(page = 1) {
   const respone = await axios.get(
-    `http://18.191.22.173:5000/api/v1/movies?sort=-ratingValue&page=${page}`
+    `http://localhost:5000/api/v1/movies?sort=-ratingValue&page=${page}`
   );
   return respone.data;
 }
