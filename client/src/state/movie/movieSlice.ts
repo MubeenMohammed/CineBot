0beebf77740de119b68ae4d2319b891e45874b54
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2025-01-22 13:34:19 -0500

    Added the Movie Slice for redux

---
+++ client/src/state/movie/movieSlice.ts
@@ -0,0 +1,64 @@
+import Movie from "@/types/movies";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getMovies } from "../../../util/backendAPICalls";
+
+interface MovieState {
+  movies: Movie[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: MovieState = {
+  movies: [
+    {
+      id: 1,
+      lastUpdated: new Date(),
+      title: "The Shawshank Redemption",
+      description:
+        "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.",
+      duration: "2h 22min",
+      ratingValue: 9.3,
+      url: "https://www.imdb.com/title/tt0111161/",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_.jpg",
+      genre: "Drama",
+      ratingCount: 2993410,
+    },
+  ],
+  loading: false,
+  error: null,
+};
+
+const MovieSlice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {
+    addMovie(state, action: PayloadAction<Movie>) {
+      state.movies.push(action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovies.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchMovies.fulfilled, (state, action) => {
+        state.loading = false;
+        state.movies = action.payload;
+      })
+      .addCase(fetchMovies.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "An error occurred.";
+      });
+  },
+});
+
+export const fetchMovies = createAsyncThunk("movie/fetchMovies", async () => {
+  const response = await getMovies();
+  return response;
+});
+
+export const { addMovie } = MovieSlice.actions;
+
+export default MovieSlice.reducer;
