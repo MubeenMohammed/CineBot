b1c735a93feaa41bab2a36ebf4b3aefa38cba3f2
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2025-01-23 12:25:47 -0500

    fixed the deployment

--- client/util/backendAPICalls.ts
+++ client/util/backendAPICalls.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getMovies(page = 1) {
   const response = await axios.get(
-    `http://localhost:5000/api/v1/movies?sort=-ratingValue&page=${page}`
+    `http://18.191.22.173:5000/api/v1/movies?sort=-ratingValue&page=${page}`
   );
   console.log(response.data.data.movies);
   return response.data.data.movies;
